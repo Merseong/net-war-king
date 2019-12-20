1503bd6b1697c95e7309bd9fa731d62611dbfa69
Merseong <esc990720@korea.ac.kr>
2019-12-20 17:50:45 +0900

    Add ChatManager and link with UI

---
+++ Assets/Scripts/ChatManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChatManager : MonoBehaviour
+{
+    public GameObject serverPrefab;
+    public GameObject clientPrefab;
+
+    public Text ipText;
+
+    public void ServerAction()
+    {
+
+    }
+
+    public void ClientAction()
+    {
+
+    }
+
+    public void DisconnectAction()
+    {
+
+    }
+}
