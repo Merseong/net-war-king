c34ab525600b2179e9c663c55f434e490942d1db
Merseong <esc990720@korea.ac.kr>
2020-01-04 21:31:35 +0900

    Add window drag

--- Assets/Scripts/NWK/MonoDragableObject.cs
+++ Assets/Scripts/NWK/MonoDragableObject.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MonoDragableObject : MonoBehaviour
 {
     [Header("Dragable Object Base")]
+    public bool useMouseOffset;
     public float xSize;
     public float ySize;
     public Vector2 ldPos
@@ -28,15 +29,27 @@ public class MonoDragableObject : MonoBehaviour
         }
     }
 
+    private bool isOffsetReady = false;
+    private Vector3 offset;
+
     public virtual void OnMouseDrag()
     {
         Vector3 mousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePoint.z = 0;
+        if (useMouseOffset)
+        {
+            if (!isOffsetReady)
+            {
+                offset = transform.position - mousePoint;
+                isOffsetReady = true;
+            }
+            mousePoint += offset;
+        }
         transform.position = mousePoint;
     }
     public virtual void OnMouseUp()
     {
-
+        isOffsetReady = false;
     }
     public bool CheckInside(Vector2 pos)
     {
